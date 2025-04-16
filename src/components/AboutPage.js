36a5ee81f209339d533cf9f602914eb98da411f0
sabinraj <sabinraj2404@gmail.com>
2025-04-16 18:31:09 +0530

    first commit

---
+++ src/components/AboutPage.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import styled, { keyframes, ThemeProvider } from 'styled-components'
+import {DarkTheme} from './Themes';
+
+
+import LogoComponent from '../subComponents/LogoComponent';
+import SocialIcons from '../subComponents/SocialIcons';
+import PowerButton from '../subComponents/PowerButton';
+import ParticleComponent from '../subComponents/ParticleComponent';
+import BigTitle from '../subComponents/BigTitlte'
+import astronaut from '../assets/Images/about.jpg'
+
+const Box = styled.div`
+background-color: ${props => props.theme.body};
+width: 100vw;
+height:100vh;
+position: relative;
+overflow: hidden;
+`
+const float = keyframes`
+0% { transform: translateY(-10px) }
+50% { transform: translateY(15px) translateX(15px) }
+100% { transform: translateY(-10px) }
+
+`
+const Spaceman = styled.div`
+position: absolute;
+top:23%;
+right: 7%;
+width: 20vw;
+animation: ${float} 3s ease infinite;
+img{
+    width: 100%;
+    height: auto;
+}
+`
+const Main =  styled.div`
+  border: 2px solid ${(props) => props.theme.text};
+  color: ${(props) => props.theme.text};
+  padding: 2rem;
+  width: 50vw;
+  height: 60vh;
+  z-index: 3;
+  line-height: 1.5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: calc(0.6rem + 1vw);
+ backdrop-filter: blur(4px);
+  
+  position: absolute;
+  left: calc(7rem + 5vw);
+  top: 9rem;
+  font-family: 'Ubuntu Mono', monospace;
+  font-style: italic;
+`
+
+
+const AboutPage = () => {
+    return (
+        <ThemeProvider theme={DarkTheme}>
+<Box>
+
+<LogoComponent theme='dark'/>
+<SocialIcons theme='dark'/>
+<PowerButton />
+<ParticleComponent theme='dark' />
+
+        <Spaceman>
+            <img src={astronaut} alt="about.png" />
+        </Spaceman>    
+        <Main>
+        Currently working as a Deputy Manager at HDFC Bank, I focus on increasing branch book size and providing financial advisory services. I also have strong knowledge of KYC processes and regulatory compliance.
+<br /> <br/>
+I'm a Business Analyst with hands-on project experience  in Power BI, Tableau, SQL, and data visualization. I specialize in gathering and analyzing stakeholder requirements to drive strategic business decisions.
+<br/> <br/>
+I believe everything is an Art when you put your consciousness in it. You can connect with me via social links.
+        </Main>
+
+        <BigTitle text="ABOUT" top="10%" left="5%" />
+
+
+        </Box>
+
+        </ThemeProvider>
+        
+    )
+}
+
+export default AboutPage
---
+++ src/data/WorkData.js
@@ -0,0 +1,61 @@
+// work data
+
+export const Work = [
+    {
+        id: 1,
+        name: "Brain Tumor Detection Using Image Processing",
+        description: "Played a key role in project by organizing millions of unstructured data into a clean,structured format. This significantly increased the project's accuracy to 90%.",
+        tags: ["image-processing", "data-cleaning", "college-project"],
+    },
+    {
+        id: 2,
+        name: "Internship at VTS Enterprises India Private Limited",
+        description: "Gained hands-on experience and corporate exposure during my internship, learning about industrial processes and real-world business practices.",
+        tags: ["internship", "corporate", "business-knowledge"],
+ 
+    },
+    {
+        id: 3,
+        name: "Deputy Manager at HDFC Bank",
+        description: "Working as a Deputy Manager, I handle financial advisory, increase branch book size, manage customer portfolios, and have strong expertise in account management and KYC processes.",
+        tags: ["banking", "finance", "account-management", "KYC"],
+    },
+    /*
+    {
+        id: 4,
+        name: "Agency Landing Page",
+        description: "Built on top of React JS, with styled-components and GSAP for smooth scrolling animations.",
+        tags: ["react", "gsap", "styledComponents"],
+        
+        demo: "https://agency-website-eta.vercel.app/",
+        github: "https://github.com/codebucks27/Agency-website"
+    },
+    {
+        id: 5,
+        name: "Job Listing Website",
+        description: "A job listing website built in ReactJS with search and filtering functionalities.",
+        tags: ["react", "sass"],
+        
+        demo: "http://react-job-listing-website.codebucks27.vercel.app/",
+        github: "https://github.com/codebucks27/react-job-listing-website"
+    },
+    {
+        id: 6,
+        name: "Dev.to Clone",
+        description: "A clone of Dev.to built using ReactJS, fetching articles in real-time from Dev.to API.",
+        tags: ["react", "api", "sass"],
+        
+        demo: "http://devto-clone-codebucks27.vercel.app/",
+        github: "https://github.com/codebucks27/devto-clone"
+    },
+    {
+        id: 7,
+        name: "Sidebar Menu",
+        description: "A cool-looking sidebar menu built using ReactJS and styled-components, with smooth animations.",
+        tags: ["react", "styledComponents", "react-router"],
+        
+        demo: "https://react-sidebar.vercel.app/",
+        github: "https://github.com/codebucks27/react-sidebar"
+    }*/
+
+];
